61422d148222468227ea808edfc7ec884748d53b
vitodursopettersson <vitodurso98@gmail.com>
2023-11-30 10:37:19 +0100

    feat: milestone 1

--- main.js
+++ main.js
@@ -1,1 +1,32 @@
 'use strict'
+
+const { createApp } = Vue;
+
+createApp({
+    data() {
+        const taskList = [
+            {
+                text: 'Vue To Do List',
+                done: false,
+            },
+            {
+                text: 'Boolzapp',
+                done: false,
+            },
+            {
+                text: 'Vue Slider',
+                done: false,
+            },
+            {
+                text: 'Vue Hello',
+                done: true,
+            },
+        ]
+        return {
+            taskList,
+        }
+    },
+    methods: {
+
+    }
+}).mount('#app')
